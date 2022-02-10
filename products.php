30feaea4053423b8488a6784ff63fa666a4e7567
saralimbu2017 <iamsebi7@gmail.com>
2022-02-10 15:43:42 +1100

    Fixing layout

--- products.php
+++ products.php
@@ -1,14 +1,4 @@
 <?php include 'includes/header.php' ?>
-<div class="menu">
-	<ul id="dc_mega-menu-orange" class="dc_mm-orange">
-	  <li><a href="index.html">Home</a></li>
-	  <li><a href="products.html">Products</a> </li>
-	  <li><a href="topbrands.html">Top Brands</a></li>
-	  <li><a href="cart.html">Cart</a></li>
-	  <li><a href="contact.html">Contact</a> </li>
-	  <div class="clear"></div>
-	</ul>
-</div>
 
  <div class="main">
     <div class="content">
