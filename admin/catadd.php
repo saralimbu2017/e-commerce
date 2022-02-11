235c9d6c66bb3155d24aa8f50b6f15401c2a17d6
saralimbu2017 <iamsebi7@gmail.com>
2022-02-11 19:38:19 +1100

    Inserting a new category is form submitted

--- admin/catadd.php
+++ admin/catadd.php
@@ -1,14 +1,24 @@
-﻿<?php include 'inc/header.php';?>
-<?php include 'inc/sidebar.php';?>
+﻿<?php 
+ include 'inc/header.php';
+ include 'inc/sidebar.php';
+ include '../classes/Category.php';
+
+ $cat = new Category();
+ if($_SERVER['REQUEST_METHOD'] == 'POST') {
+     $catName = $_POST['catName'];
+     $insertCat =  $cat->catInsert($catName);
+ }
+ ?>
+
         <div class="grid_10">
             <div class="box round first grid">
                 <h2>Add New Category</h2>
                <div class="block copyblock"> 
-                 <form>
+                 <form method="post" action="">
                     <table class="form">					
                         <tr>
                             <td>
-                                <input type="text" placeholder="Enter Category Name..." class="medium" />
+                                <input type="text" name="catName" placeholder="Enter Category Name..." class="medium" />
                             </td>
                         </tr>
 						<tr> 
