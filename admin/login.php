5fe04f2b9603275bf90437ca80ca76977fd8731d
saralimbu2017 <iamsebi7@gmail.com>
2022-02-11 16:37:03 +1100

    Renaming input fields

--- admin/login.php
+++ admin/login.php
@@ -1,27 +1,50 @@
+<?php 
+	include '../classes/Adminlogin.php';
+		//creating adminLogin object
+		$admLogin = new AdminLogin();
+
+		//checking if Post method is submitted
+		if($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$adminUser = $_POST['adminUser'];
+			$adminPass = $_POST['adminPass'];
+	
+			//user authentication
+			$loginCheck = $admLogin->adminLogin($adminUser, $adminPass);
+		}
+	
+	
+	?>
+
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
 <title>Login</title>
     <link rel="stylesheet" type="text/css" href="css/stylelogin.css" media="screen" />
 </head>
-<body>
+<body> 
 <div class="container">
 	<section id="content">
-		<form action="" method="post">
+		<form action="login.php" method="post">
 			<h1>Admin Login</h1>
+			<span style="color:red; font-size: 18px;">
+		  <?php
+			//display the respective message after checking user input
+				if(isset($loginCheck)) {
+					echo $loginCheck;
+				}
+			?>
+			</span>
 			<div>
-				<input type="text" placeholder="Username" required="" name="username"/>
+				<input type="text" placeholder="Username" required="" name="adminUser"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="password"/>
+				<input type="password" placeholder="Password" required="" name="adminPass"/>
 			</div>
 			<div>
 				<input type="submit" value="Log in" />
 			</div>
 		</form><!-- form -->
-		<div class="button">
-			<a href="#">Training with live project</a>
-		</div><!-- button -->
+		<!-- button -->
 	</section><!-- content -->
 </div><!-- container -->
 </body>
