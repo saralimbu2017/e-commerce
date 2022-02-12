c5516e48f04440cae3e1a8029a64a00900a2b392
saralimbu2017 <iamsebi7@gmail.com>
2022-02-12 11:04:31 +1100

    Displaying message after inserting data

--- classes/Category.php
+++ classes/Category.php
@@ -1,6 +1,6 @@
 <?php
-  include '../lib/Database.php';
-  include '../helpers/Format.php';
+  include_once '../lib/Database.php';
+  include_once '../helpers/Format.php';
 
   class Category {
     private $db;
@@ -9,6 +9,30 @@
     public function __construct() {
       $this->db = new Database();
       $this->fm = new Format();
+    }
+
+    //insert new category to database
+    public function catInsert($catName) {
+      $catName = $this->fm->validation($catName);
+      $catName = mysqli_real_escape_string($this->db->link, $catName);
+      if(empty($catName)) {
+        $msg = "Category Field must not be empty. ";
+        return $msg;
+      } else {
+        //insert query
+        $query = "INSERT INTO tbl_category(catName) values('$catName')";
+        $catinsert = $this->db->insert($query);
+        //if successful to insert data display data to user
+        if($catinsert) {
+          $msg = "<span class='success'> Category inserted</span>";
+          return $msg;
+         
+        } else {
+          //Display message on failure to insert data
+          $msg = "<span class='error'> Category failed to be inserted</span>";
+          return $msg;
+        }
+      }
     }
 
   }
