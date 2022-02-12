d68156281bfd99e7b4950a92562a104bfb4b4e11
saralimbu2017 <iamsebi7@gmail.com>
2022-02-12 22:15:59 +1100

    Fixing indentation

--- admin/catlist.php
+++ admin/catlist.php
@@ -12,48 +12,53 @@
 	}
 ?>
 	<div class="grid_10">
-			<div class="box round first grid">
-					<h2>Category List</h2>
-					<div class="block">        
-							<table class="data display datatable" id="example">
-		<thead>
-			<tr>
-				<th>Serial No.</th>
-				<th>Category Name</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php 
-				//get all the categories list
-				$getCategory = $cat->getAllCaterogies();
-				if($getCategory) {
-					$i = 0;
-					//loop through the categories
-					while($result = $getCategory->fetch_assoc()) {
-					$i++;
-				
-			?>
-			<tr class="odd gradeX">
-				<td><?php echo $i; ?></td>
-				<td><?php echo $result['catName']; ?></td>
-				<td><a href="catedit.php?catId=<?php echo $result['catId']; ?>">Edit</a> || 
-				<a onclick="return confirm('Are you sure to delete')" href="?delcat=<?php echo $result['catId']; ?>">Delete</a></td>
-			</tr>
-			<?php  	
+		<div class="box round first grid">
+			<h2>Category List</h2>
+			<div class="block">  
+				<?php
+					if(isset($delCat)) {
+						echo $delCat;
 					}
-				} ?>
-		</tbody>
-	</table>
-					</div>
+				?>
+				<table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Category Name</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+							//get all the categories list
+							$getCategory = $cat->getAllCaterogies();
+							if($getCategory) {
+								$i = 0;
+								//loop through the categories
+								while($result = $getCategory->fetch_assoc()) {
+								$i++;
+				
+						?>
+						<tr class="odd gradeX">
+							<td><?php echo $i; ?></td>
+							<td><?php echo $result['catName']; ?></td>
+							<td><a href="catedit.php?catId=<?php echo $result['catId']; ?>">Edit</a> || 
+							<a onclick="return confirm('Are you sure to delete')" href="?delcat=<?php echo $result['catId']; ?>">Delete</a></td>
+						</tr>
+						<?php  	
+								}
+							} ?>
+					</tbody>
+				</table>
+				</div>
 			</div>
 	</div>
 	<script type="text/javascript">
 		$(document).ready(function () {
-				setupLeftMenu();
+			setupLeftMenu();
 
-				$('.datatable').dataTable();
-				setSidebarHeight();
+			$('.datatable').dataTable();
+			setSidebarHeight();
 		});
 	</script>
 <?php include 'inc/footer.php';?>
