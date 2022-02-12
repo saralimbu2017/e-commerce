d80f2ba64f82b78f93122baa9311a5e5784c0419
saralimbu2017 <iamsebi7@gmail.com>
2022-02-12 18:51:47 +1100

    Updated indentation

--- admin/catadd.php
+++ admin/catadd.php
@@ -1,39 +1,39 @@
 ﻿<?php 
- include_once 'inc/header.php';
- include_once 'inc/sidebar.php';
- include_once '../classes/Category.php';
+    include_once 'inc/header.php';
+    include_once 'inc/sidebar.php';
+    include_once '../classes/Category.php';
 
- $cat = new Category();
- if($_SERVER['REQUEST_METHOD'] == 'POST') {
-     $catName = $_POST['catName'];
-     $insertCat =  $cat->catInsert($catName);
- }
+    $cat = new Category();
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $catName = $_POST['catName'];
+        $insertCat =  $cat->catInsert($catName);
+    }
  ?>
 
-        <div class="grid_10">
-            <div class="box round first grid">
-                <h2>Add New Category</h2>
-               <div class="block copyblock"> 
-              <?php 
-                    if(isset($insertCat)) {
-                        echo $insertCat;
-                    }
-                ?> 
-                 <form method="post" action="">
+    <div class="grid_10">
+        <div class="box round first grid">
+            <h2>Add New Category</h2>
+            <div class="block copyblock"> 
+            <?php 
+                if(isset($insertCat)) {
+                    echo $insertCat;
+                }
+            ?> 
+                <form method="post" action="">
                     <table class="form">					
                         <tr>
                             <td>
                                 <input type="text" name="catName" placeholder="Enter Category Name..." class="medium" />
                             </td>
                         </tr>
-						<tr> 
+                        <tr> 
                             <td>
                                 <input type="submit" name="submit" Value="Save" />
                             </td>
                         </tr>
                     </table>
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
+    </div>
 <?php include 'inc/footer.php';?>
