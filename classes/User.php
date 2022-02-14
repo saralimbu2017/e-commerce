6611b1ba9d4552643630beba086f8bdac25b077e
saralimbu2017 <iamsebi7@gmail.com>
2022-02-14 18:29:36 +1100

    Declaring User class

---
+++ classes/User.php
@@ -0,0 +1,14 @@
+<?php
+  include_once '../lib/Database.php';
+  include_once '../helpers/Format.php';
+
+  class User {
+    private $db;
+    private $fm;
+
+    public function __construct() {
+      $this->db = new Database();
+      $this->fm = new Format();
+    }
+  }
+?>
