c4c5a6d75f1bba5aaee22089f3464a55e4cfe041
saralimbu2017 <iamsebi7@gmail.com>
2022-02-16 08:29:56 +1100

    Adding getCartProduct method

--- classes/Cart.php
+++ classes/Cart.php
@@ -12,5 +12,37 @@
       $this->db = new Database();
       $this->fm = new Format();
     }
+
+    public function addToCart($quantity, $id) {
+      $quantity = $this->fm->validation($quantity);
+      $quantity = mysqli_real_escape_string($this->db->link, $quantity);
+      $productId = mysqli_real_escape_string($this->db->link, $id);
+      $sId = session_id();
+
+      $squery = "SELECT * FROM tbl_product WHERE productId = '$productId'";
+      $result = $this->db->select($squery)->fetch_assoc();
+
+      $productName = $result['productName'];
+      $price = $result['price'];
+      $image = $result['image'];
+
+      $query = "INSERT INTO tbl_cart(sId, productId, productName, price, quantity, image)   VALUES('$sId','$productId','$productName','$price','$quantity','$image')";
+      $inserted_row = $this->db->insert($query);
+
+      if($inserted_row) {
+        header("Location:cart.php");
+
+      } else {
+        header("Location:404.php");
+      }
+    }
+
+    //Reading product details from database
+    public function getCartProduct() {
+      $sId = session_id();
+      $query = "SELECT * FROM tbl_cart WHERE productId = '$sId' ";
+      $result = $this->db->select($query);
+      return $result;
+    }
   }
 ?>
