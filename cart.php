a023c8697d2b233a953458ec6e82a65d5138c6d8
saralimbu2017 <iamsebi7@gmail.com>
2022-02-16 11:22:49 +1100

    Displaying products added in cart

--- cart.php
+++ cart.php
@@ -8,6 +8,7 @@
 			    	<h2>Your Cart</h2>
 						<table class="tblone">
 							<tr>
+								<th width="20%">Item No.</th>
 								<th width="20%">Product Name</th>
 								<th width="10%">Image</th>
 								<th width="15%">Price</th>
@@ -18,11 +19,13 @@
 							<?php
 									$getProd = $cart->getCartProduct();
 									if($getProd) {
+										$i = 0;
 										while($result = $getProd->fetch_assoc()) {
 											$i++;
 									
 							?>
 							<tr>
+
 								<td><?php echo $i;?></td>
 								<td><?php echo $result['productName'];?></td>
 								<td><img src="admin/<?php echo $result['image'];?>" alt=""/></td>
